5828e35c04c746f522f6e9b45348bfe126295a5d
karnampraveen <168419934+karnampraveen@users.noreply.github.com>
2024-09-05 17:34:42 +0100

    Update App.js

--- LegacyCodeFE/src/App.js
+++ LegacyCodeFE/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './pages/Navbar';
 import HomePage from './pages/HomePage';
 import CartPage from './pages/CartPage';
