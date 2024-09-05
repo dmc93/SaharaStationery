f7a5b079d85aa45ebad7afe2842a540ee7edd327
karnampraveen <168419934+karnampraveen@users.noreply.github.com>
2024-09-05 17:11:03 +0100

    Updated App.js for Login related routes

--- LegacyCodeFE/src/App.js
+++ LegacyCodeFE/src/App.js
@@ -9,6 +9,17 @@ import OrderHistory from './pages/OrderHistory';
 import { CartProvider } from './components/CartContext'; 
 import Footer from './components/Footer';
 
+// import Security pages
+
+import LoginPage from './security/LoginPage';
+import RegistrationPage from './security/RegistrationPage';
+import UpdateUser from './security/UpdateUser';
+import UserService from './security/UserService';
+import UserManagementPage from './security/UserManagementPage';
+import ProfilePage from './security/ProfilePage';
+import Chatbot from './components/Chatbot';
+// import Security pages end
+
 
 function App() {
   return (
@@ -17,10 +28,26 @@ function App() {
         <CartProvider>
           <Navbar />
           <Routes>
-            <Route path="/" element={<HomePage />} />
+  {/*            <Route path="/" element={<HomePage />} />
             <Route path="/shop" element={<ShopPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/admin" element={<Admin />} />
+  */}
+            <Route exact path="/" element={<LoginPage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/user-management" element={<UserManagementPage />} />
+            <Route path="/update-user/:userId" element={<UpdateUser />} />
+            <Route path="/product" element={<Admin />} />
+
+            <Route path="/chat" element={<Chatbot />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+    
             <Route path="/order-history" element={<OrderHistory />} /> 
           </Routes>
         </CartProvider>
